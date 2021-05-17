4af1811f16bede393cd7be7fad0aeecfadc421ef
Hairul Amirin <19041390@myrp.edu.sg>
2021-05-17 18:13:44 +0800

    First commit test

---
+++ TSHFYPWebPortal/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using TSHFYPWebPortal.Models;
+using Microsoft.AspNetCore.Authorization;
+using System.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TSHFYPWebPortal.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            DataTable dt = DBUtl.GetTable("SELECT * FROM PurchaseOrder");
+            return View("Index", dt.Rows);
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new Orders { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ TSHFYPWebPortal/Models/Orders.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace TSHFYPWebPortal.Models
+{
+    public class Orders
+    {
+        public int Pid { get; }
+        public int sts { get; }
+        public int Bflag { get; }
+
+        public DateTime OrderDate { get; set; }
+        public DateTime DueDate { get; set; }
+    }
+}
