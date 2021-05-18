e5aec52ee84fd4839e8f5dcf4ad3738d4c2cf2fa
Hairul Amirin <19041390@myrp.edu.sg>
2021-05-18 14:28:56 +0800

    Sample PO table

---
+++ TSHFYPWebPortal2/Controllers/OrderController.cs
@@ -0,0 +1,34 @@
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using TSHFYPWebPortal2.Models;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace TSHFYPWebPortal2.Controllers
+{
+public class OrderController : Controller
+{
+   
+    public IActionResult About()
+    {
+        return View();
+    }
+
+    
+    public IActionResult Index()
+    {
+        DataTable dt = DBUtl.GetTable("SELECT * FROM PurchaseOrder");
+        return View("Index", dt.Rows);
+
+    }
+
+
+
+    
+  
+
+
+}
+}
---
+++ TSHFYPWebPortal2/Models/Order.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace TSHFYPWebPortal2.Models
+{
+   public class Order
+   {
+        public int Pid { get; set; }
+        public int sts { get; set; }
+        public int Bflag { get; set; }
+
+
+
+        [DataType(DataType.DateTime)]
+        public DateTime OrderDate { get; set; }
+        public DateTime DueDate { get; set; }
+
+        public DateTime RevDelDate { get; set; }
+        public string POnum { get; set; }
+        public string PRnum { get; set; }
+        public string SupplierID { get; set; }
+        public string SupplierName { get; set; }
+        public string Description { get; set; }
+        public int Qty { get; set; }
+    }
+
+}
